620d368dfa5999d81a3d4c81cf4ddf6a7591caad
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-06 14:40:06 -0300

    create function to render projects in the UI

--- src/js/ui.js
+++ src/js/ui.js
@@ -4,6 +4,7 @@ import Project from './project';
 
 const UI = (() => {
   const btnCreateProject = document.getElementById('create-project');
+  const projectsUser = document.getElementById('projects-user');
 
   const createProject = () => {
     const nameProject = document.getElementById('project-name').value;
@@ -11,7 +12,22 @@ const UI = (() => {
       const newProject = Project({ title: nameProject });
       storage.create(newProject.title, newProject);
     }
+    renderProjects();
   };
+
+  const renderProjects = () => {
+    let projects = storage.allNamesProjects().sort();
+    projectsUser.innerHTML = '';
+
+    projects.forEach(project => {
+      const content = `
+      <div>
+        <span>${project}</span>
+      </div>
+      `;
+      projectsUser.insertAdjacentHTML('beforeend', content);
+    })
+  }
 
 
   const loadListeners = () => {
