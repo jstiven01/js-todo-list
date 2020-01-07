f33d80aff2111094e3f6ed4f389d8ab9df403ab3
jstiven01 <stiven10@gmail.com>
2020-01-07 17:57:20 -0500

    Fixing Stickler errors

--- src/js/project.js
+++ src/js/project.js
@@ -1,5 +1,5 @@
 const Project = ({ title, tasks = [] }) => {
-  const findTask = (titleTask) => tasks.filter((task) => task.title === titleTask)[0];
+  const findTask = titleTask => tasks.filter(task => task.title === titleTask)[0];
   return {
     title, tasks, findTask,
   };
